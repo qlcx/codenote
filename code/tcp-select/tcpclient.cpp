bd512e9587983f5f424a7596ba1d309b2e296de3
qlcx <1136425019@qq.com>
2016-02-07 21:33:19 +0800

    shutdown

--- code/tcp-select/tcpclient.cpp
+++ code/tcp-select/tcpclient.cpp
@@ -14,10 +14,10 @@ using namespace std;
 int main(void)
 {
 	//1.创建socket,使用socket函数
-	int sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(-1 == sockfd)
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (-1 == sockfd)
 	{
-		perror("socket"),exit(-1);
+		perror("socket"), exit(-1);
 	}
 	printf("创建socket成功\n");
 	//2.准备通信地址，服务器地址
@@ -26,31 +26,36 @@ int main(void)
 	addr.sin_port = htons(8888);
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	//3.连接socket和通信地址,connect
-	int res = connect(sockfd,(struct sockaddr*)&addr,sizeof(addr));
-	if(-1 == res)
+	int res = connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+	if (-1 == res)
 	{
-		perror("connect"),exit(-1);
+		perror("connect"), exit(-1);
 	}
 	printf("连接服务器成功\n");
 	//4.不断地和服务器进行通信
-	while(1)
+	while (1)
 	{
-		char buf[100] = {0};
+		char buf[100] = { 0 };
 		printf("请输入要发送的内容：\n");
-		scanf("%s",buf);
-		res = send(sockfd,buf,sizeof(buf), 0);
-		if(-1 == res)
+		scanf("%s", buf);
+		res = send(sockfd, buf, sizeof(buf), 0);
+		if (-1 == res)
 		{
-			perror("send"),exit(-1);
+			perror("send"), exit(-1);
 		}
-
-		char buf2[100] = {0};
-		res = recv(sockfd,buf2,sizeof(buf2), 0);
-		if(-1 == res)
+		
+		char buf2[100] = { 0 };
+		res = recv(sockfd, buf2, sizeof(buf2), 0);
+		if (-1 == res)
 		{
-			perror("recv"),exit(-1);
+			perror("recv"), exit(-1);
 		}
-		printf("服务器发来的消息是：%s\n",buf2);
+		if(0 == res)
+		{
+			cout << "close socket" << endl;
+			break;
+		}
+		printf("服务器发来的消息是：%s\n", buf2);
 	}
 	//5.关闭socket,使用close函数
 	close(sockfd);
--- code/tcp-select/tcpserver.cpp
+++ code/tcp-select/tcpserver.cpp
@@ -12,72 +12,81 @@ using namespace std;
 
 int main()
 {
-	int serv_sock,clnt_sock;
+	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_adr, clnt_adr;
 	struct timeval timeout;
 	fd_set reads, cpy_reads;
-	
+
 	socklen_t adr_sz;
-	
+
 	int fd_max, str_len, fd_num, i;
 	char buf[1024];
+
+	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_adr.sin_port = htons(8888);
+
+	//解决地址被占用的问题
+	int reuseaddr = 1;
+	setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr));
 	
-	serv_sock=socket(AF_INET, SOCK_STREAM, 0);
-	memset(&serv_adr, 0,sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=inet_addr("127.0.0.1");
-	serv_adr.sin_port=htons(8888);
-	
-	if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
 		perror("bind");
-	if(listen(serv_sock, 5) == -1)
+	if (listen(serv_sock, 5) == -1)
 		perror("listen");
-	
+
 	FD_ZERO(&reads);
 	FD_SET(serv_sock, &reads);
-	fd_max=serv_sock;
-	
-	while(1)
+	fd_max = serv_sock;
+
+	while (1)
 	{
-		cpy_reads=reads;
-		timeout.tv_sec=5;
-		timeout.tv_usec=0;
-		
-		if((fd_num=select(fd_max+1, &cpy_reads, 0, 0, &timeout))==-1)
+		cpy_reads = reads;
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+
+		if ((fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout)) == -1)
 			break;
-		if(fd_num==0)
+		if (fd_num == 0)
 			continue;
-		for(i=0;i<fd_max+1;i++)
-		{	
-			if(FD_ISSET(i, &cpy_reads))
+		
+		for (i = 0; i<fd_max + 1; i++)
+		{
+			if (FD_ISSET(i, &cpy_reads))
 			{
-				if(i==serv_sock)
+				if (i == serv_sock)
 				{
-					adr_sz=sizeof(clnt_adr);
-					clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+					adr_sz = sizeof(clnt_adr);
+					clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
 					FD_SET(clnt_sock, &reads);
-					if(fd_max<clnt_sock)
-						fd_max=clnt_sock;
+					if (fd_max<clnt_sock)
+						fd_max = clnt_sock;
 					printf("connected client: %d \n", clnt_sock);
-				}	
+				}
 				else
 				{
-					str_len=recv(i, buf, sizeof(buf), 0);
-					if(str_len==0)
+					str_len = recv(i, buf, sizeof(buf), 0);
+					if (str_len == 0)
 					{
 						FD_CLR(i, &reads);
 						close(i);
-						printf("close client: %d \n", i);	
+						printf("close client: %d \n", i);
 					}
 					else
 					{
 						cout << "发送来的数据为：" << buf << endl;
-					}
-                    send(i, "ok!", 3, 0);
+					}	
+					int send_len = send(i, "ok!", 3, 0);
+					cout << "send_len: " << send_len << endl;
+					//半关闭连接(关闭套接字下一次的send操作)
+					//shutdown(i, SHUT_WR);
+					//shutdown(i, SHUT_RD);//(关闭套接字下一次的recv操作)
 				}
 			}
-		}		
+		}
 	}
 	close(serv_sock);
-	return 0;	
+	return 0;
 }
